b00cf8c2da6259f683e269eaaa3272f5aa7cc251
Krzysztof <krzysztof.majda@tricholab.com>
2021-10-22 16:55:00 +0200

    add inline for BinaryOperator class

--- ParserTask/BinaryOperator.h
+++ ParserTask/BinaryOperator.h
@@ -7,8 +7,8 @@ class BinaryOperator :
     public IASTNode
 {
 public:
-    BinaryOperator(std::unique_ptr<IASTNode> leftArg, std::unique_ptr<IASTNode> rightArg);
-    int Evaluate() const override;
+    inline BinaryOperator(std::unique_ptr<IASTNode> leftArg, std::unique_ptr<IASTNode> rightArg);
+    inline int Evaluate() const override;
 private:
     std::unique_ptr<IASTNode> _leftArg;
     std::unique_ptr<IASTNode> _rightArg;
@@ -19,20 +19,20 @@ private:
 
 
 template<typename Op>
-BinaryOperator<typename Op>::BinaryOperator(std::unique_ptr<IASTNode> leftArg, std::unique_ptr<IASTNode> rightArg) :
+inline BinaryOperator<typename Op>::BinaryOperator(std::unique_ptr<IASTNode> leftArg, std::unique_ptr<IASTNode> rightArg) :
     _leftArg(std::move(leftArg)),
     _rightArg(std::move(rightArg))
 {
 }
 
 template<typename Op>
-int BinaryOperator<Op>::Evaluate() const
+inline int BinaryOperator<Op>::Evaluate() const
 {
     return Op()(_leftArg->Evaluate(), _rightArg->Evaluate());
 }
 
 template<>
-int BinaryOperator<std::divides<void>>::Evaluate()const
+inline int BinaryOperator<std::divides<void>>::Evaluate()const
 {
     int leftValue = _leftArg->Evaluate();
     int rightValue = _rightArg->Evaluate();
